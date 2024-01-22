631cc7f6cd32c2795a587730774460917291d7ee
Lautaro Companys <lautarocompanys@gmail.com>
2024-01-22 13:54:21 -0300

    Angular-Lautaro-Companys-Primera-Pre-Entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { DashboardModule } from './layouts/dashboard/dashboard.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent,ReactiveFormsComponent,],
+  imports: [BrowserModule,AppRoutingModule, ReactiveFormsModule, BrowserAnimationsModule,DashboardModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/layouts/dashboard/pages/students/students.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { alumnos } from './models/students';
+
+
+
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.scss'
+})
+export class StudentsComponent {
+  displayedColumns: string[] = ['id', 'name','apellido', 'edad', 'asistencia'];
+  dataSource: alumnos[] = [
+    {
+      id: 1,
+      nombre: "Lautaro",
+      apellido:"Companys",
+      edad: 25,
+      asistencia: true,
+    },
+    {
+      id: 2,
+      nombre: "Emanuel",
+      apellido:"Ginobilli",
+      edad: 46,
+      asistencia: false,
+    },
+    {
+      id: 3,
+      nombre: "Lebron",
+      apellido:"James",
+      edad: 39,
+      asistencia: true,
+    }
+  ];
+
+  onAlumnosSubmited(ev: alumnos): void{
+    this.dataSource = [...this.dataSource, ev]
+  }
+
+}
